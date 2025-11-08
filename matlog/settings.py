496d3499fc78ee2af29cc191aaf9c66db7eeddfe
Voinov Vladyslav <voinovvladyslav@gmail.com>
2025-11-08 07:19:18 +0200

    fix: correct user model, format with ruff

--- matlog/settings.py
+++ matlog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
-    "django-insecure-k_(av03n^6!(sh7(o+!0=+gpz4(m*vy6f0*$npj7m2u^$v3z6("
+    'django-insecure-k_(av03n^6!(sh7(o+!0=+gpz4(m*vy6f0*$npj7m2u^$v3z6('
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -33,53 +33,53 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "core",
-    "users",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',
+    'users',
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "matlog.urls"
+ROOT_URLCONF = 'matlog.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "matlog.wsgi.application"
+WSGI_APPLICATION = 'matlog.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -89,16 +89,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
     },
 ]
 
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -118,11 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,19 +1,23 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 from core.models import TimeStampedModel
 
 
 class UserRole(models.TextChoices):
-    STUDENT = "student", "student"
-    TEACHER = "teacher", "teacher"
-    ADMIN = "admin", "admin"
+    STUDENT = 'student', 'student'
+    TEACHER = 'teacher', 'teacher'
+    ADMIN = 'admin', 'admin'
 
 
 class UserManager(BaseUserManager):
     def create_user(
         self, email: str, password: str | None = None, **extra_fields
-    ) -> "User":
+    ) -> 'User':
         user: User = self.model(
             email=self.normalize_email(email), **extra_fields
         )
@@ -25,7 +29,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(
         self, email: str, password: str | None = None, **extra_fields
-    ) -> "User":
+    ) -> 'User':
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -33,11 +37,12 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(TimeStampedModel, AbstractBaseUser):
+class User(PermissionsMixin, TimeStampedModel, AbstractBaseUser):
     email = models.EmailField(unique=True)
-    first_name = models.CharField(blank=True, default="")
-    last_name = models.CharField(blank=True, default="")
-    middle_name = models.CharField(blank=True, default="")
+    first_name = models.CharField(blank=True, default='')
+    last_name = models.CharField(blank=True, default='')
+    middle_name = models.CharField(blank=True, default='')
+    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     role = models.CharField(
@@ -46,4 +51,5 @@ class User(TimeStampedModel, AbstractBaseUser):
         default=UserRole.STUDENT,
     )
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
+    objects = UserManager()

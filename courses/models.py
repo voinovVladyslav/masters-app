0005277e76aacbf33643d374c1e53e20f2154946
Voinov Vladyslav <voinovvladyslav@gmail.com>
2025-11-08 07:59:31 +0200

    feat: lecture materials and courses models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+class Course(TimeStampedModel):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, default='')
+
+    owner = models.ForeignKey(
+        'users.User',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='owned_courses',
+    )
+    students = models.ManyToManyField(
+        'users.User',
+        blank=True,
+        related_name='courses',
+    )
+
+
+class Theme(TimeStampedModel):
+    name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True, default='')
+    order = models.PositiveSmallIntegerField(default=0)
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStampedModel
+
+
+def material_upload_to(instance: 'Material', filename: str) -> str:
+    return 'files/'
+
+
+class Material(TimeStampedModel):
+    name = models.CharField(max_length=200)
+    theme = models.ForeignKey(
+        'courses.Theme',
+        on_delete=models.CASCADE,
+        related_name='materials',
+    )
+    file = models.FileField(upload_to=material_upload_to)

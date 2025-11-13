db4f3560a2a8392e6418746090244fea3145db16
Voinov Vladyslav <voinovvladyslav@gmail.com>
2025-11-13 12:54:13 +0200

    feat: user create serializer

--- api/v1/users/urls.py
+++ api/v1/users/urls.py
@@ -5,4 +5,5 @@ from api.v1.users import views
 urlpatterns = [
     path('', views.UserListApi.as_view(), name='user-list'),
     path('self/', views.UserSelfApi.as_view(), name='user-self'),
+    path('create/', views.UserCreateApi.as_view(), name='user-create'),
 ]
--- api/v1/users/views.py
+++ api/v1/users/views.py
@@ -1,15 +1,16 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
-from rest_framework.generics import GenericAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView, ListAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from api.v1.pagination import DefaultPagination
-from api.v1.users.serializers import UserDisplaySerializer
+from api.v1.users.serializers import UserCreateSerializer, UserDisplaySerializer
 from users.models import User
 
 
 class UserListApi(ListAPIView):
-    queryset = User.objects.all()
+    queryset = User.objects.order_by('-id')
     serializer_class = UserDisplaySerializer
     pagination_class = DefaultPagination
 
@@ -26,4 +27,21 @@ class UserSelfApi(GenericAPIView):
         user = self.get_object()
         return Response(
             UserDisplaySerializer(user).data, status=status.HTTP_200_OK
+        )
+
+
+class UserCreateApi(CreateAPIView):
+    input_serializer_class = UserCreateSerializer
+    output_serializer_class = UserDisplaySerializer
+
+    @extend_schema(
+        request=UserCreateSerializer,
+        responses={status.HTTP_201_CREATED: UserDisplaySerializer},
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = UserCreateSerializer(data=request.data)
+        serializer.is_valid()
+        user = serializer.save()
+        return Response(
+            UserDisplaySerializer(user).data, status=status.HTTP_201_CREATED
         )

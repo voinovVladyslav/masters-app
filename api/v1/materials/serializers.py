826419f4cfc98692d8e48dab3f7860236c06d360
Voinov Vladyslav <voinovvladyslav@gmail.com>
2025-11-14 14:30:22 +0200

    feat: materials and themes serializeres

---
+++ api/v1/materials/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from materials.models import Material
+
+
+class MaterialDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = (
+            'id',
+            'theme',
+            'name',
+            'file',
+        )
--- api/v1/themes/serializers.py
+++ api/v1/themes/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
 
+from api.v1.materials.serializers import MaterialDisplaySerializer
 from courses.models import Theme
 
 
 class ThemeDisplaySerializer(serializers.ModelSerializer):
+    materials = MaterialDisplaySerializer(many=True, read_only=True)
+
     class Meta:
         model = Theme
         fields = (
             'id',
+            'course',
             'name',
             'description',
             'order',
-            'course',
             'created_at',
+            'materials',
         )

832f88479df6e45f7f1be8265da557e34f02d0d7
Danylo Morhun <danymorhun@gmail.com>
2025-11-20 14:58:44 +0100

    feat: add static files configuration

--- matlog/urls.py
+++ matlog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -29,3 +31,7 @@ urlpatterns = [
         name='swagger-ui',
     ),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
